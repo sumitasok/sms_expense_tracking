59ee7489dcd17cef5bb2bfcb1afcdeaad802cfd8
Sumit Asok <sumitasok@gmail.com>
2020-04-03 11:43:20 +0530

    export csv to db and back; monthly analysis.

--- download_csv.py
+++ download_csv.py
@@ -16,16 +16,13 @@ if __name__ == "__main__":
 
     collection = mongoCollection(os.environ.get('MONGODB'), 'smsinfo', 'transactions')
 
-    monthly = list(collection.find({'status': {'analysis_done': True},
+    monthly = list(collection.find({'status.analysis_done': True,
                 'transaction.datetime' : {
                     '$gte': datetime.datetime(year,month,1), '$lt': datetime.datetime(next_year,next_month,1)}
-                }, {'transaction': 1}))
+                }, {'transaction': 1, 'event' : 1}))
 
     monthly = nested_to_record(monthly, sep='.')
 
     dataframe = pd.DataFrame(monthly)
 
-    # values = {'transaction.available_balance': 0, 'transaction.outstanding_amount': 1, 'C': 2, 'D': 3}
-    # dataframe.fillna(value=values)
-
-    dataframe.to_csv('./data/csv.csv', index=False)
+    dataframe.to_csv("./data/{date}-csv.csv".format(date = str(datetime.datetime.now()).replace(' ','T').replace(':','-')), index=False)
